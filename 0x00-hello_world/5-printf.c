b93e65640d8d9300dfb0667d94fe09d17463e20d
bela <betenatefera@gmail.com>
2021-08-26 16:57:33 +0300

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main()
 {
-	printf("with proper grammar, but the outcome is a priece of art,
+	printf("with proper grammar, but the outcome is a priece of art,\n";
 return 0;}
