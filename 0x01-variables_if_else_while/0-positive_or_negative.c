421eae23be56366309300b472c0c794abf429fa8
bela <betenatefera@gmail.com>
2021-08-27 16:37:42 +0300

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,16 @@
+#!/bin/bash
+#include <studio.h>
+
+int main()
+{
+	int n;
+	cin>>n;
+	if(n>0)
+	{
+		printf("is positive");
+	else if(n=0)
+		printf("is zero");
+	else printf("is negative");
+	}
+return 0;
+	}
