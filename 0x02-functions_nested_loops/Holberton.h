c49d66c6df522cba9aee5f5917da9d2c3c41ab3a
beyaye <you@example.com>
2021-08-31 12:21:54 +0300

    2

--- 0x02-functions_nested_loops/Holberton.h
+++ 0x02-functions_nested_loops/Holberton.h
@@ -1,1 +1,12 @@
-void print_alphabet(void);
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
