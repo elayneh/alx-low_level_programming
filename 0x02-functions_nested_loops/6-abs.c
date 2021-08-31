9e86dbdbfe6703e2bbeda4c6ee3a5f2af43c03aa
beyaye <you@example.com>
2021-09-01 02:43:16 +0300

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *_abs - print the absolute value an integer.
+ *@a: integer to be printed.
+ *Return: absolute value of a.
+ */
+int _abs(int a)
+{
+	return (a * (a > 0) - (a < 0));
+}
