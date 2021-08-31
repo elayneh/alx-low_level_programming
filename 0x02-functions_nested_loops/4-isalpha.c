8e688ba233b82ece666e75325d14e9bc301e1837
beyaye <you@example.com>
2021-09-01 02:15:14 +0300

    fifth question

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,18 @@
 #include "main.h"
 /**
-*main - check the code for ALX School students.
-*
-* return: Always 0.
-*/
-int main(void)
+ *_isalpha - Determine whether an integer is character or not.
+ *@c: the number to be checked.
+ * Return: 1 if character, o if otherwise.
+ */
+int _isalpha(int c)
 {
-	char a;
+	if ((a >= 'a' && a <= 'z') || (a >= 'A' && a <= 'Z'))
+	{
+		_putchar('a');
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
