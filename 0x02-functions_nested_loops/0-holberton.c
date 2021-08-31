f8d90c2171e2ffe32ceb544cdf2d34fc32fcf39c
beyaye <you@example.com>
2021-08-31 10:23:38 +0300

    3

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+{
+	char *name = "Holberton";
+
+	while (*name)
+	{
+		putchar(*name);
+		name++;
+	}
+	putchar('\n');
+
+	return (0);
+}
