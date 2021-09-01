22dc970ae56f706282b4b4eafd03f9e750c3223c
beyaye <you@example.com>
2021-09-01 04:50:41 +0300

    9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,18 +1,39 @@
-#include "main.h"
+#include "holberton.h"
 /**
  * times_table -  a function that prints the 9 times table, starting with 0
  * rone = row, cone = column, d = digits of current result
  * Return: times table
  * add extra space past single digit
  */
-void times_table(int)
+void times_table(void)
 {
-	for (int h = 0; h < 9; h++)
+	int rone, cone, d;
+
+	for (rone = 0; rone <= 9; rone++)
 	{
-	for (int m = 0; m <= 9; m++)
-	{
-		_putchar(h * m + ' ');
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 1; cone <= 9; cone++)
+		{
+			d = (rone * cone);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (cone < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
 	}
-	return ('\n');
-	}
+
 }
