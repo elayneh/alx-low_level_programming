1d461723c56d1dd2713fcd0abbea9fdee26e8dfd
beyaye <you@example.com>
2021-09-01 04:19:04 +0300

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 /**
  * jack_bauer - function that prints every minute of the day, 24 hour clock
  * h = hour, m = minutes
@@ -5,10 +6,20 @@
  * for loop breaks before passing 24:00
  * Return: 24 hour clock line by line
  */
-void jack_bauer(int m)
+void jack_bauer(void)
 {
-	for (m = '1'; m <= '24'; ++m)
+	int h, m;
+
+	for (h = 0; h < 24; h++)
 	{
-		_putchar('m');
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
 	}
 }
