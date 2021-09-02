50d4790fe94518b9ca2508e35b2c4f2a9bb0e1d1
beyaye <you@example.com>
2021-09-02 11:33:14 +0300

    2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,11 +2,6 @@
 #define HOLBERTON_H
 
 #include <stdio.h>
-2-main.c
-3-convert_day.c
-3-main_a.c
-3-main_b.c
-
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
