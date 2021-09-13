34c6874aebd4c5817b5992793b9bd34f0fa055cd
beya <betenatefera@gmail.com>
2021-09-13 09:00:54 +0300

    modified

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,22 +1,20 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-  * print_chessboard - Print the chessboard
-  * @a: array of pieces
-  *
-  * Return: Nothing.
-  */
+ * print_chessboard - locates a character in a string
+ * @a: is a pointer type char
+ * Return: The number of bytes repeated
+ */
 void print_chessboard(char (*a)[8])
 {
-	int b, c;
-
-	for (b = 0; b < 8; b++)
-	{
-		for (c = 0; c < 8; c++)
-		{
-			_putchar(a[b][c]);
-		}
-
-		_putchar('\n');
-	}
+int i, j;
+for (i = 0; i < 8; i++)
+{
+for (j = 0; j < 8; j++)
+{
+_putchar(a[i][j]);
+}
+_putchar('\n');
+}
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,22 +2,30 @@
 #include <stdio.h>
 
 /**
-  * print_diagsums - Print the sum of two diagonals of square matrix
-  *
-  * @a: the matrix
-  * @size: the size
-  *
-  * Return: Nothing.
-  */
+ * print_diagsums - locates a character in a string
+ * @a: is a pointer type char
+ * @size: is a variable type size
+ * Return: Nothing
+ */
 void print_diagsums(int *a, int size)
 {
-	int b, sum1 = 0, sum2 = 0;
+int i, j = 0, sum1 = 0, sum2 = 0;
 
-	for (b = 0; b < size; b++)
-	{
-		sum1 += a[(size + 1) * b];
-		sum2 += a[(size - 1) * (b + 1)];
-	}
+for (i = 0; i < size; i++)
+{
+sum1 += a[j];
+j += size + 1;
+}
 
-	printf("%d, %d\n", sum1, sum2);
+j = 0;
+
+j = size - 1;
+
+for (i = 0; i < size; i++)
+{
+sum2 += a[j];
+j += size - 1;
+}
+
+printf("%d, %d\n", sum1, sum2);
 }
