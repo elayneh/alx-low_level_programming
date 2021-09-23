1c0c284cf5ac515e3ba4eb57399154c64d9b9278
beya <betenatefera@gmail.com>
2021-09-23 11:01:24 +0300

    files are added

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,50 +1,52 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * string_nconcat - ...
- * @s1: the first string to be concatinated
- * @s2: the second string to be concatinated
- * @n: the first n bytes in s2
- *
- * Return: the concatenated string
- */
+  * string_nconcat - ...
+  * @s1: ...
+  * @s2: ...
+  * @n: ...
+  *
+  * Return: ...
+  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, k = 0;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *str;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	if (n >= sizeof(s2))
-	{
-		j = sizeof(s1) + sizeof(s2);
-	}
-	else
-	{
-		j = sizeof(s1) + n;
-	}
 
-	str = malloc(sizeof(char) * j + 1);
+	while (s1[i])
+		i++;
+
+	while (s2[k])
+		k++;
+
+	if (n >= k)
+		l = i + k;
+	else
+		l = i + n;
+
+	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < j; i++)
+	k = 0;
+	while (j < l)
 	{
-		if (i <= sizeof(s1))
+		if (j <= i)
+			str[j] = s1[j];
+
+		if (j >= i)
 		{
-			str[i] = s1[i];
-		}
-		if (i >= sizeof(s1))
-		{
-			str[i] = s2[k];
+			str[j] = s2[k];
 			k++;
 		}
+		j++;
 	}
 	str[j] = '\0';
 	return (str);
 }
-
