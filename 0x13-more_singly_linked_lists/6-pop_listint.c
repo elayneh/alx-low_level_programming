be32c1337be3f7416d3ad13e2d688f7b732fc221
belayneh getachew <betenatefera@gmail.com>
2021-10-11 17:30:41 -0400

    files on more linked list project is added

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,29 @@
+/*
+ * File: 6-pop_listint.c
+ * Auth: Belayneh Getachew
+ */
+
+#include "lists.h"
+
+/**
+ * pop_listint - Deletes the head node of a listint_t list.
+ * @head: A pointer to the address of the
+ *        head of the listint_t list.
+ *
+ * Return: If the linked list is empty - 0.
+ *         Otherwise - The head node's data (n).
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int rest;
+
+	if (*head == NULL)
+		return (0);
+
+	temp = *head;
+	rest = (*head)->next;
+
+	free(temp);
+	return (rest);
+}
