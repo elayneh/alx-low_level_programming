3e4411c185a927770eda7b0cc60206c41422f641
belayneh getachew <betenatefera@gmail.com>
2021-10-21 12:03:04 -0400

    file to create a c-file is added

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -3,14 +3,14 @@
 #include "main.h"
 
 /**
- * create_file - function that creates a file.
+ * append_text_to_file - that appends text at the end of a file.
  * @filename: variable pointer
  * @text_content: content file
- * Description: Create a function that creates a file.
- * Return: 1 on success, -1 on failure
+ * Description: function that appends text at the end of of a file
+ * Return: -1 on failure, 1 on success
  */
 
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int i = 0, file;
 
@@ -20,13 +20,12 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-
 	while (text_content[i] != '\0')
 	{
 		i++;
 	}
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	file = open(filename, O_WRONLY | O_APPEND);
 
 	if (file == -1)
 		return (-1);
@@ -35,4 +34,3 @@ int create_file(const char *filename, char *text_content)
 
 	return (1);
 }
-
