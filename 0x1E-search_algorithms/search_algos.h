1b57be9a71cfe1df62fcd9af5311c4b547ebfec0
elayneh <betenatefera@gmail.com>
2022-03-29 17:59:09 -0400

    add all of the given prototypes

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,3 +1,8 @@
+#ifndef SEARCH_H
+#define SEARCH_H
 #include <stdio.h>
-#include <stdlib.h>
+
+/* Prototypes*/
 int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
